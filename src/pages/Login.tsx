0c857ec2d60d38267118ae448ef37a911faa6854
Amitmeel01 <meelamit354@gmail.com>
2024-08-13 10:39:49 +0530

    update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,23 @@
+import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom"; // Import Link from React Router DOM
 import AppBar from "../components/AppBar";
 import google from "../assets/google.svg";
 import Footer from "../components/Footer";
 
 function Login() {
-    const Navigate = useNavigate()
-    const token = localStorage.getItem('token')
-    if(token){
-        Navigate('/')
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (token) {
+      navigate("/");
     }
-    if(!token){
-      Navigate('/login')
-  }
+  }, [token, navigate]);
+
   const handleGoogleLogin = () => {
     // Redirect to Google login URL
-    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reach-inbox-assignment-mu.vercel.app/";
+    window.location.href =
+      "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reach-inbox-assignment-phi.vercel.app/";
   };
 
   return (
@@ -23,11 +26,9 @@ function Login() {
       <div className="bg-black text-white w-screen h-screen flex flex-col justify-center items-center">
         <div className="bg-[#111214] text-center space-y-10 px-16 rounded-2xl border border-[#343A40]">
           <div className="">
-            <div className="text-xl font-semibold mt-6">
-              Create a new account
-            </div>
+            <div className="text-xl font-semibold mt-6">Create a new account</div>
             <div
-              className="mt-6 border-white/40 border px-20 py-2 text-sm  flex items-center text-[#CCCCCC] rounded-lg cursor-pointer"
+              className="mt-6 border-white/40 border px-20 py-2 text-sm flex items-center text-[#CCCCCC] rounded-lg cursor-pointer"
               onClick={handleGoogleLogin} // Call handleGoogleLogin function onClick
             >
               <img src={google} alt="google" className="w-4 mr-3"></img>
@@ -36,7 +37,7 @@ function Login() {
           </div>
           <div className="">
             <Link
-              to="/login" 
+              to="/login"
               className="bg-gradient-to-r from-[#4B63DD] to-[#0524BFFC] mx-16 mt-5 px-6 text-sm py-3 rounded-md cursor-pointer"
             >
               Create an Account
