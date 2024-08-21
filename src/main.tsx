d910b5fc3bd833fe53928a36c1710b5a03d7f90c
Amitmeel01 <meelamit354@gmail.com>
2024-08-22 00:55:06 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -10,14 +10,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const CLIENT_ID = "877729925712-p6buhqv1emn357pttln722ss75b0bojq.apps.googleusercontent.com"; // Ensure this is set in your .env file
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
+    
       <App />
-    </GoogleOAuthProvider>
+   
   </React.StrictMode>
 );
